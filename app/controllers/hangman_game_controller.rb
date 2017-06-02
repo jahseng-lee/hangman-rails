eface96a1c0cb502f7ab7664b7c62c1127ba2e7d
Jah Seng Lee <jahseng.lee@gmail.com>
2017-06-02 15:15:03 +1200

    base files for HangmanGame

---
+++ app/controllers/hangman_game_controller.rb
@@ -0,0 +1,2 @@
+class HangmanGameController < ApplicationController
+end
---
+++ test/controllers/hangman_game_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HangmanGameControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
