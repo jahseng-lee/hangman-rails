a4e57bf57d0652da3c09ba11b5db7f92907f0199
Jah Seng Lee <jahseng.lee@gmail.com>
2017-06-06 13:48:33 +1200

    added validators for hangman game

--- app/models/hangman_game.rb
+++ app/models/hangman_game.rb
@@ -1,2 +1,9 @@
 class HangmanGame < ApplicationRecord
+  validates :mystery_word, presence: true, length: { minimum: 2 }
+  validates :lives, presence: true, numericality: { greater_than: 0 }
+  validate :mystery_word_is_alphabetical
+
+  def mystery_word_is_alphabetical
+    errors.add(:mystery_word, 'is not alphabetical') unless mystery_word.eql?(/^[A-Za-z]+$/)
+  end
 end
