a057ca3176b509fe0c8d6152178edc7f9e20bc80
Jah Seng Lee <jahseng.lee@gmail.com>
2017-06-06 16:46:15 +1200

    implemented the rest of logic

--- app/models/hangman_game.rb
+++ app/models/hangman_game.rb
@@ -2,7 +2,7 @@ class HangmanGame < ApplicationRecord
   after_initialize :set_guesses
 
   validates :mystery_word, :mystery_word_is_alphabetical, length: { minimum: 2 }
-  validates :lives, numericality: { greater_than: 0 }
+  validates :initial_lives, numericality: { greater_than: 0 }
   validates :mystery_word, :lives, presence: true
 
   def set_guesses
@@ -13,9 +13,17 @@ class HangmanGame < ApplicationRecord
     errors.add(:mystery_word, 'is not alphabetical') unless mystery_word.eql?(/^[A-Za-z]+$/)
   end
 
+  def guess(input)
+    guesses << input.downcase
+  end
+
   def masked_word
     mystery_word.chars.map do |c|
-      c ? c : nil
+      if guesses.include? c.downcase
+        c
+      else
+        nil
+      end
     end
   end
 
@@ -33,6 +41,25 @@ class HangmanGame < ApplicationRecord
   end
 
   def lives
-    initial_lives - guesses.length
+    initial_lives - incorrect_guesses.length
+  end
+
+  def won?
+    mystery_word.chars.eql? masked_word
+  end
+
+  def running?
+    lives > 0 && !won?
+  end
+
+  private
+
+  def incorrect_guesses
+    # NOTE compact feels really gross
+    guesses.chars.map do |c|
+      if mystery_word.downcase.exclude? c
+        c
+      end
+    end.compact
   end
 end
