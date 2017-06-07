9e81c52b89fb43053ca2631c1acc3910d140b32d
Jah Seng Lee <jahseng.lee@gmail.com>
2017-06-08 10:01:09 +1200

    implemented new hangman game

--- app/controllers/hangman_game_controller.rb
+++ app/controllers/hangman_game_controller.rb
@@ -1,4 +1,9 @@
 class HangmanGameController < ApplicationController
   def new
+    if @game = HangmanGame.create(
+        mystery_word: HangmanGameHelper.random_word,
+        initial_lives: HangmanGameHelper::INITIAL_LIVES)
+      render 'game'
+    end
   end
 end
