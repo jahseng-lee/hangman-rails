c562f1bca1379fb4c6fb185dbb3947e91dcc0e51
Jah Seng Lee <jahseng.lee@gmail.com>
2017-06-08 15:35:30 +1200

    changed new to create and updated to more idiomatic code

--- app/controllers/hangman_game_controller.rb
+++ app/controllers/hangman_game_controller.rb
@@ -1,9 +1,13 @@
 class HangmanGameController < ApplicationController
-  def new
-    if @game = HangmanGame.create(
-        mystery_word: HangmanGameHelper.random_word,
-        initial_lives: HangmanGameHelper::INITIAL_LIVES)
-      render 'game'
+  def create
+    @game = HangmanGame.new(
+      mystery_word: HangmanGameHelper.random_word,
+      initial_lives: HangmanGameHelper::INITIAL_LIVES)
+
+    if @game.save
+      redirect_to @game
+    else
+      flash[:error] = "Couldn't save"
     end
   end
 
@@ -13,6 +17,5 @@ class HangmanGameController < ApplicationController
 
   def show
     @game = HangmanGame.find(params[:id])
-    render 'game'
   end
 end
