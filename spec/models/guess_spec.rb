87df9c218f36f565b9b8ffe44fd03c409c9fe4b7
Jah Seng Lee <jahseng.lee@gmail.com>
2017-06-12 14:11:29 +1200

    removed specs which tested rails

--- spec/models/guess_spec.rb
+++ spec/models/guess_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Guess do
   let(:char) { 'f' }
   let(:hangman_game_id) { 1 }
   let(:game) { HangmanGame.create(mystery_word: 'doesntmatter', initial_lives: 1) }
-  # NOTE replace all hangman_game_id with game
 
   describe 'testing guess validation' do
     context 'with invalid input' do
@@ -38,53 +37,6 @@ RSpec.describe Guess do
     context 'with valid input' do
       it 'saves' do
         expect(Guess.create(char: 'f', hangman_game_id: game.id)).to be_valid
-      end
-    end
-  end
-
-  describe 'testing guesses' do
-    let(:hangman_game_id) { 1 } # NOTE this is repeated above, ask mentors if this adds clarity
-
-    context 'before any guesses have been made' do
-      it 'returns an empty array' do
-        # TODO shouldn't need to explicitly pass game id, can throw object in?
-        # NOTE game.guesses?
-        expect(Guess.guesses_for(hangman_game_id)).to eql([])
-      end
-    end
-
-    context 'with some guesses' do
-
-      it 'returns all guesses made in this game' do
-        Guess.create(char: 'a', hangman_game_id: hangman_game_id)
-        Guess.create(char: 'b', hangman_game_id: hangman_game_id)
-        Guess.create(char: 'c', hangman_game_id: hangman_game_id)
-
-        expect(Guess.guesses_for(hangman_game_id)).to eql([ 'a', 'b', 'c' ])
-      end
-    end
-
-    context 'with guesses from other games' do
-      let(:other_hangman_game_id) { 2 }
-
-      it 'doesn\'t return any guesses from other games' do
-        Guess.create(char: 'a', hangman_game_id: other_hangman_game_id)
-        Guess.create(char: 'b', hangman_game_id: other_hangman_game_id)
-        Guess.create(char: 'c', hangman_game_id: other_hangman_game_id)
-
-        expect(Guess.guesses_for(hangman_game_id)).to eql([])
-      end
-
-      it 'only returns guesses from this game' do
-        Guess.create(char: 'a', hangman_game_id: hangman_game_id)
-        Guess.create(char: 'b', hangman_game_id: hangman_game_id)
-        Guess.create(char: 'c', hangman_game_id: hangman_game_id)
-
-        Guess.create(char: 'd', hangman_game_id: other_hangman_game_id)
-        Guess.create(char: 'e', hangman_game_id: other_hangman_game_id)
-        Guess.create(char: 'f', hangman_game_id: other_hangman_game_id)
-
-        expect(Guess.guesses_for(hangman_game_id)).to eql([ 'a', 'b', 'c' ])
       end
     end
   end
