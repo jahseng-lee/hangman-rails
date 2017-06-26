90b82bb8035f4b0d5e0e650f2de6a1ca359eb1b5
Jah Seng Lee <jahseng.lee@gmail.com>
2017-06-27 10:15:37 +1200

    updated validators in guess to not take uppercase char

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,7 +2,7 @@ class Guess < ApplicationRecord
   belongs_to :hangman_game
 
   validate :hangman_game_running
-  validates_format_of :char, with: /\A[[:alpha:]]\Z/
+  validates_format_of :char, with: /\A[a-z]\Z/
   validates_presence_of :char
   validates_uniqueness_of :char, scope: :hangman_game_id, message: 'already guessed'
 
