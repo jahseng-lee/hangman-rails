58027247b394f7cd6c400fcb3ef2574c86189621
Jah Seng Lee <jahseng.lee@gmail.com>
2017-06-27 11:19:43 +1200

    new 'in' scope for correct guesses

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -6,6 +6,7 @@ class Guess < ApplicationRecord
   validates_presence_of :char
   validates_uniqueness_of :char, scope: :hangman_game_id, message: 'already guessed'
 
+  scope :in, ->(word) { where("char IN (?)", word.chars) }
   scope :not_in, ->(word) { where("char NOT IN (?)", word.chars) }
 
   private
