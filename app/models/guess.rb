842c0ba9bde13704756451727c7ce6d8eb0ca60a
Jah Seng Lee <jahseng.lee@gmail.com>
2017-06-27 10:05:15 +1200

    fixed query for not_in scope

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,14 +1,16 @@
 class Guess < ApplicationRecord
   belongs_to :hangman_game
 
-  validate :game_is_running
+  validate :hangman_game_running
   validates_format_of :char, with: /\A[[:alpha:]]\Z/
-  validates_presence_of :char, :hangman_game_id
+  validates_presence_of :char
   validates_uniqueness_of :char, scope: :hangman_game_id, message: 'already guessed'
+
+  scope :not_in, ->(word) { where("char NOT IN (?)", word.chars) }
 
   private
 
-  def game_is_running
+  def hangman_game_running
     hangman_game.running? || errors.add(:hangman_game, "is over")
   end
 end
